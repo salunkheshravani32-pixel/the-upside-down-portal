07e6f1e2ca8fd5b7894dd47f0387cd8a2cf62952
Shravani Salunkhe <salunkheshravani32@gmail.com>
2026-07-21 19:58:03 +0530

    Connect frontend to deployed backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://the-upside-down-portal.onrender.com/api",
 });
 
 export default API;
